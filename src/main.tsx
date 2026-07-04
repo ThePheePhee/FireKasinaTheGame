26573c8004eae2e13a4bfde1fdb5ff10ecb4b459
ThePheePhee <niffen@gmail.com>
2026-07-04 17:10:23 +1200

    Add touch map gestures and pinch zoom

--- src/main.tsx
+++ src/main.tsx
@@ -4,4 +4,5 @@ import App from './App';
 import './styles.css';
 import './mobile.css';
 import './dialogue.css';
+import './mapControls.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
