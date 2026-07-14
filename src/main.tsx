4617da4823753ac326d836788f5a4dfbf36fe3e2
ThePheePhee <niffen@gmail.com>
2026-07-14 16:22:56 +1200

    Overhaul Mists minigames

--- src/main.tsx
+++ src/main.tsx
@@ -5,4 +5,5 @@ import './styles.css';
 import './mobile.css';
 import './dialogue.css';
 import './mapControls.css';
+import './minigameOverrides.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
